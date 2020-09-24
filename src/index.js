05c8ec63fd8d0de7ffa3ddb096ba59d6ace0e22d
Adam Morgan <adamcodes716@gmail.com>
2020-09-24 08:42:59 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+require('./models/User');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://dbUser:password2password@cluster0.c3xnz.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoUri, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+})
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+    console.log('error connecting to mongo', err);
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Hi there!'); 
+});
+
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+})
